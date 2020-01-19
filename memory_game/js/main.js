51c948fee65c0cb0a7b9a55d282f777c1ea98339
Janis Rancourt <janis.rancourt@gmail.com>
2020-01-19 16:02:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,35 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = [
+	"queen",
+	"queen",
+	"king",
+	"king"
+];
 
-console.log("User flipped " + cardOne);
+const cardsInPlay = [
+
+];
+
+let cardOne = cards[3];
+cardsInPlay.push(cardOne);
+
+console.log("User first flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User next flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	switch (cardsInPlay[0] === cardsInPlay[1]) {
+		case true:
+			alert("You found a match!");
+			break;
+		case false:
+			alert("Sorry, try again.");
+			break;
+		default:
+			alert("an error occurred");
+	}
+}
